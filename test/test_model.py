5684bd8075b6247f2b99b6c233189c9090feb07e
linyc74 <yclin.python@gmail.com>
2026-05-06 21:47:45 +0800

    fix unittest

--- test/test_model.py
+++ test/test_model.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from test.setup import TestCase
-from linfeat.model import cast_to_appropriate_type, cast_to_categorical
+from linfeat.model import cast_to_appropriate_type
 
 
 class TestFunctions(TestCase):
@@ -24,29 +24,8 @@ class TestFunctions(TestCase):
             ('False', 'False'),
         ]:
             actual = cast_to_appropriate_type(value)
-            print(f'value: {value}, expected: {expected}, actual: {actual}')
-            print(f'type(value): {type(value)}, type(expected): {type(expected)}, type(actual): {type(actual)}')
-            return
             if pd.isna(expected):
                 self.assertTrue(pd.isna(actual))
             else:
                 self.assertEqual(actual, expected)
                 self.assertEqual(type(actual), type(expected))
-
-    def test_cast_to_categorical(self):
-        for value, expected in [
-            (np.nan, np.nan),
-            ('', np.nan),
-            (None, np.nan),
-            ('123', '123'),
-            (123, '123'),
-            (123.456, '123.456'),
-            (True, 'True'),
-            (False, 'False'),
-            ('nan', 'nan'),
-        ]:
-            actual = cast_to_categorical(value)
-            if pd.isna(expected):
-                self.assertTrue(pd.isna(actual))
-            else:
-                self.assertEqual(actual, expected)
